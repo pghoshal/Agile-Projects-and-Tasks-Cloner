a1820343e77694f123082572d3ef769e225e11f2
prasenjit.ghoshal@seamless.se <innovativeprasen@gmail.com>
2016-08-26 23:41:35 +0530

    Initial commit

---
+++ src/main/java/com/jira/plugin/clone/task/AddonApplication.java
@@ -0,0 +1,12 @@
+package com.jira.plugin.clone.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddonApplication {
+
+    public static void main(String[] args) throws Exception {
+        new SpringApplication(AddonApplication.class).run(args);
+    }
+}
