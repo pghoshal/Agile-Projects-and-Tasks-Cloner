2b4a01d72ec1acd83f99bfce17e3547ecdea477c
prasenjit.ghoshal@seamless.se <innovativeprasen@gmail.com>
2016-08-30 22:52:24 +0530

    Design changed

--- src/main/java/com/jira/plugin/clone/task/CloneTaskController.java
+++ src/main/java/com/jira/plugin/clone/task/CloneTaskController.java
@@ -3,10 +3,14 @@ package com.jira.plugin.clone.task;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +24,7 @@ import com.atlassian.connect.spring.IgnoreJwt;
 import com.atlassian.jira.rest.client.api.JiraRestClient;
 import com.atlassian.jira.rest.client.api.domain.BasicProject;
 import com.atlassian.jira.rest.client.api.domain.Issue;
+import com.atlassian.jira.rest.client.api.domain.IssueType;
 import com.atlassian.jira.rest.client.api.domain.SearchResult;
 import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
 import com.atlassian.util.concurrent.Promise;
@@ -28,6 +33,7 @@ import com.atlassian.util.concurrent.Promise;
 @IgnoreJwt
 public class CloneTaskController
 {
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private RestTemplate restTemplate;
 
@@ -53,10 +59,23 @@ public class CloneTaskController
 	    
         final JiraRestClient restClient = factory.createWithBasicHttpAuthentication(jiraServerUri, "admin", "123456789");
         Iterator<BasicProject> iterator = restClient.getProjectClient().getAllProjects().claim().iterator();
-        Promise<SearchResult> issues = restClient.getSearchClient().searchJql("project=TD");
+        Promise<SearchResult> issues = restClient.getSearchClient().searchJql("project=TD", 50000, 0, null);
+        Set<String> commonIssueTypes=new HashSet<String>();
+        Set<String> customIssueTypes=new HashSet<String>();
         try {
 			SearchResult searchResult = issues.get();
 			issueTypeList = (List<Issue>) searchResult.getIssues();
+			for (Issue issue : issueTypeList)
+			{
+				if(issue.getIssueType().getName().equalsIgnoreCase("Story")||issue.getIssueType().getName().equalsIgnoreCase("Bug")||issue.getIssueType().getName().equalsIgnoreCase("Sub-task"))
+					{
+					commonIssueTypes.add(issue.getIssueType().getName());
+					
+					}else{
+						customIssueTypes.add(issue.getIssueType().getName());
+					}
+					log.info(issue.getIssueType().getName());
+			}
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		} catch (ExecutionException e) {
@@ -70,6 +89,9 @@ public class CloneTaskController
         	projectList.add(item);
         }
 	    model.setViewName("copyIssue");
+	    model.addObject("commonIssueTypes",commonIssueTypes);
+	    model.addObject("customIssueTypes",customIssueTypes);
+	    
 	    model.addObject("projectList", projectList);
 	    model.addObject("issueTypeList",issueTypeList);
 	    return model;
